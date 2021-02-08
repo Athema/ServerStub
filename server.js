458a5a412cf53f99a7ea5dbf52ad09579eaec478
Aleix <avilacanela@gmail.com>
2021-02-08 12:05:33 +0100

    herokuFirst

---
+++ server.js
@@ -0,0 +1,8 @@
+const io = require('socketio')() //doble parentesi instancia
+
+io.on('connection', client => {
+    console.log("client: " + client.id);
+})
+
+io.listen(process.env.PORT || 3000)
+    // 3000 en cas de que Heroku no la portés per defecte, 3000 és local
