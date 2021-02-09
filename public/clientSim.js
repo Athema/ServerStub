18a7702a1974c115f79657899038317bb02285a4
Aleix <avilacanela@gmail.com>
2021-02-09 11:56:13 +0100

    database added

--- public/clientSim.js
+++ public/clientSim.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //const socket = io('localhost:3000')
-    const   socket = io('localhost:3000')
-    // const socket = io('https://fast-spire-68417.herokuapp.com/')
+    //const   socket = io('localhost:3000')
+    const socket = io('https://fast-spire-68417.herokuapp.com/')
 
     socket.on('salut', missatge => {
         console.log(missatge);
